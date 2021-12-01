02389488dc508736b8d79537bfab2a139d44a860
ajl <cnlarryan@foxmail.com>
2021-12-01 10:16:55 +0800

    最基础的多线程，另开线程去处理HTTP请求

--- TCP_Server.cpp
+++ TCP_Server.cpp
@@ -2,6 +2,7 @@
 #include<string>
 // #include<fstream>
 
+#include<pthread.h>
 #include<fcntl.h>
 #include<arpa/inet.h>
 #include<string.h>
@@ -11,44 +12,12 @@
 
 using namespace std;
 
-int main(){
-    int sock_fd, new_fd;
-    
-    struct sockaddr_in my_addr;
-    struct sockaddr_in other_addr;
-
-    if((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-        perror("socket");
-        exit(1);
-    }
-
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(12345);
-    // my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    inet_pton(AF_INET, "10.0.4.11", &my_addr.sin_addr.s_addr);
-    bzero(&(my_addr.sin_zero), 8);
-
-    if(bind(sock_fd, reinterpret_cast<struct sockaddr*>(&my_addr), sizeof(struct sockaddr)) == -1){
-        perror("bind");
-        exit(1);
-    }
-
-    if(listen(sock_fd, 10) == -1){
-        perror("listen");
-        exit(1);
-    }
-
-    unsigned int sin_size;
-    while(1){
-        sin_size = sizeof(struct sockaddr_in);
-        if((new_fd = accept(sock_fd, reinterpret_cast<struct sockaddr*>(&other_addr), &sin_size)) == -1){
-            perror("accept");
-            continue;            
-        }
-        
-        cout << "server: got connection from " << inet_ntoa(other_addr.sin_addr) << endl;
-
-
+void* httpRecvandSend(void *arg){
+    int new_fd = *((int*) arg);
+    char recv_buf[65535];
+    while(recv(new_fd, recv_buf, 65535, 0)){
+        std::cout << recv_buf << std::endl;
+        memset(recv_buf, 0, 65535);
         string send_buf;
         //响应报文头部处理开始
         string header;
@@ -92,17 +61,60 @@ int main(){
         // send_buf += string(file_buf, file_buf + read_size);
         send_buf += file_buf;
 
-        char recv_buf[65535];
-        if(!fork()){
-            recv(new_fd, recv_buf, 65535, 0);
-            std::cout << recv_buf << std::endl;    
-            if(send(new_fd, send_buf.c_str(), strlen(send_buf.c_str()), 0) == -1){
-                perror("send");
-                close(new_fd);
-                exit(0);
-            }
+        if(send(new_fd, send_buf.c_str(), strlen(send_buf.c_str()), 0) == -1){
+            perror("send");
             close(new_fd);
+            exit(0);
         }
+    }   
+    close(new_fd);
+    return nullptr;
+}
+
+
+
+int main(){
+    int sock_fd, new_fd;
+    
+    struct sockaddr_in my_addr;
+    struct sockaddr_in other_addr;
+
+    if((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("socket");
+        exit(1);
+    }
+
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(12345);
+    // my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    inet_pton(AF_INET, "10.0.4.11", &my_addr.sin_addr.s_addr);
+    bzero(&(my_addr.sin_zero), 8);
+
+    if(bind(sock_fd, reinterpret_cast<struct sockaddr*>(&my_addr), sizeof(struct sockaddr)) == -1){
+        perror("bind");
+        exit(1);
+    }
+
+    if(listen(sock_fd, 10) == -1){
+        perror("listen");
+        exit(1);
+    }
+
+    unsigned int sin_size;
+    while(1){
+        sin_size = sizeof(struct sockaddr_in);
+        if((new_fd = accept(sock_fd, reinterpret_cast<struct sockaddr*>(&other_addr), &sin_size)) == -1){
+            perror("accept");
+            continue;            
+        }
+        cout << "server: got connection from " << inet_ntoa(other_addr.sin_addr) << endl;
+
+        pthread_t t_id;
+        if(pthread_create(&t_id, NULL, httpRecvandSend, (void*)&new_fd) != 0){
+            perror("pthread_create");
+            exit(1);
+        }
+
     }
     while(waitpid(-1, NULL, WNOHANG) > 0);
 }
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 TCP_Server:TCP_Server.cpp
-	g++ -o TCP_Server TCP_Server.cpp
+	g++ -o TCP_Server TCP_Server.cpp -lpthread
 clean:
 	rm -f TCP_Server
